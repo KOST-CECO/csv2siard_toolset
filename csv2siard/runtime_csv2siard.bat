20c48b2f1edc169218579e090e47106d0b5ec8fe
u1942 <u1942@d8cf526a-ed47-ab48-8470-8a9acf7b5d78>
2011-07-14 12:50:45 +0000

    20110209
    git-svn-id: svn://localhost/Transferprojekt@16 d8cf526a-ed47-ab48-8470-8a9acf7b5d78

--- csv2siard/runtime_csv2siard.bat
+++ csv2siard/runtime_csv2siard.bat
@@ -18,16 +18,33 @@ RMDIR %RUNTIME%
 MKDIR %RUNTIME%
 
 COPY csv2siard.exe %RUNTIME%
+COPY php4ts.dll %RUNTIME%
+
 COPY xmllint.exe %RUNTIME%
-COPY sablot.dll %RUNTIME%
-COPY expat.dll %RUNTIME%
 COPY iconv.dll %RUNTIME%
-COPY 7z.* %RUNTIME%
+COPY libxml2.dll %RUNTIME%
+COPY zlib1.dll %RUNTIME%
+
+REM COPY php_xslt.dll %RUNTIME%
+REM COPY sablot.dll %RUNTIME%
+REM COPY expat.dll %RUNTIME%
+
+COPY 7z.exe %RUNTIME%
+REM COPY 7z.dll %RUNTIME%
+REM COPY gdiplus.dll %RUNTIME%
+
 COPY _*.x* %RUNTIME%
+
 COPY *.prefs %RUNTIME%
 
+REM test -----------------------------------------------------------------------
 @ECHO ON
 CD %RUNTIME%
 DEL /Q ..\test.siard
 CALL csv2siard.exe
 CALL csv2siard.exe ..\table2-model.xml ..\csvdata ..\test.siard
+
+@ECHO OFF
+REM zip ------------------------------------------------------------------------
+CD ..
+7z.exe a -mx9 %RUNTIME%.zip %RUNTIME%
