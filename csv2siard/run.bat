49637ff873688ae4911db9cf36988a79e7a79d96
U80789367 <U80789367@d8cf526a-ed47-ab48-8470-8a9acf7b5d78>
2012-01-12 16:23:25 +0000

    Handbuch mit den Erweiterungen von 1.6.2 ergänzt
    git-svn-id: svn://localhost/Transferprojekt@72 d8cf526a-ed47-ab48-8470-8a9acf7b5d78

--- csv2siard/run.bat
+++ csv2siard/run.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 DEL /Q test.siard
 echo --------- %time%
-csv2siard.exe datatype-model.xml csvtest test.siard csvtest/csvtest.prefs
+php.exe csv2siard.php datatype-model.xml csvtest test.siard csvtest/csvtest.prefs
 REM php.exe csv2siard.php NO_DB_MODEL csvtest test.siard csvtest/csvtest.prefs
 
 REM php.exe csv2siard.php NO_DB_MODEL csvtest test.siard
@@ -13,7 +13,8 @@ REM csv2siard.exe NO_DB_MODEL ../9_Testdaten/csvdata_TG test.siard
 echo --------- %time%
 @ECHO OFF
 if %ERRORLEVEL% == 0 (
-	CALL "C:\Program Files\Java\jre6\bin\javaw.exe" -jar "C:\Software\siardsuite_1.26\bin\SiardEdit.jar"
+	REM SIARD v.1.20 kann in MS Access importieren (Fehler in v.1.26)
+	CALL "C:\Program Files\Java\jre6\bin\javaw.exe" -jar "C:\Software\siardsuite_1.20\bin\SiardEdit.jar"
 )
 exit /b
 
