b7bbd1238e985b4a9837aac8e7dd9570e003fa8d
U80789367 <U80789367@d8cf526a-ed47-ab48-8470-8a9acf7b5d78>
2012-02-20 12:40:04 +0000

    start Version 1.6.4
    git-svn-id: svn://localhost/Transferprojekt@84 d8cf526a-ed47-ab48-8470-8a9acf7b5d78

--- csv2siard/run.bat
+++ csv2siard/run.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
-DEL /Q test.siard gvtg.siard gvtg(vertraulich).siard
+DEL /Q *.siard
 echo --------- %time%
+csv2siard.exe gv-model-v9.xml csvdata gvtg.siard csvdata\gvtg.prefs
+
+REM csv2siard.exe gv-model-v9.xml P:\KOST\Pilotloesungen\Gebaeudeversicherung\4_GV-Viewer\csvdata P:\KOST\Pilotloesungen\Gebaeudeversicherung\4_GV-Viewer\gvtg.siard
+REM csv2siard.exe gv-model-v9.xml P:\KOST\Pilotloesungen\Gebaeudeversicherung\9_Testdaten\csvdata_TG(vertraulich) P:\KOST\Pilotloesungen\Gebaeudeversicherung\4_GV-Viewer\gvtg(vertraulich).siard
 REM csv2siard.exe NO_DB_MODEL P:\KOST\Pilotloesungen\Gebaeudeversicherung\9_Testdaten\csvdata_TG(vertraulich) gvtg(vertraulich).siard
-csv2siard.exe gv-model-v9.xml P:\KOST\Pilotloesungen\Gebaeudeversicherung\9_Testdaten\csvdata_TG(vertraulich) gvtg(vertraulich).siard
+REM csv2siard.exe gv-model-v9.xml P:\KOST\Pilotloesungen\Gebaeudeversicherung\9_Testdaten\csvdata_TG(vertraulich) gvtg(vertraulich).siard
 REM csv2siard.exe gv-model-v9.xml csvdata gvtg.siard
 REM php.exe csv2siard.php datatype-model.xml csvtest test.siard csvtest/csvtest.prefs
 REM php.exe csv2siard.php NO_DB_MODEL csvtest test.siard csvtest/csvtest.prefs
@@ -13,11 +17,10 @@ REM php.exe csv2siard.php gv-model-v9.xml csvdata test.siard
 REM php.exe csv2siard.php NO_DB_MODEL csvdata test.siard
 REM csv2siard.exe gv-model-v9.xml ../9_Testdaten/csvdata_TG gvtg.siard
 REM csv2siard.exe NO_DB_MODEL ../9_Testdaten/csvdata_TG test.siard
-echo --------- %time%
+
 @ECHO OFF
 if %ERRORLEVEL% == 0 (
 	REM SIARD v.1.20 kann in MS Access importieren (Fehler in v.1.26)
 	CALL "C:\Program Files\Java\jre6\bin\javaw.exe" -jar "C:\Software\siardsuite_1.20\bin\SiardEdit.jar"
 )
 exit /b
-
